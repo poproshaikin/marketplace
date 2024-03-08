16f024f1a4f39dff6ff40c219552f3658f477a0f
poproshaikin <150174304+poproshaikin@users.noreply.github.com>
2024-03-08 08:48:27 +0100

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import './App.css';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./Components/Home";
+import Navigation from "./Components/Navigation";
 
 function App() {
     return (
         <div>
             <Router>
+                <Navigation/>
                 <Routes>
                     <Route path="/" element={<Home/>} />
                 </Routes>
---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const config = {
+    serverIp: "10.0.1.126",
+    serverPort: 5432
+}
