fc3153832bce1194b182ca5e53edaaf41a4a9eee
poproshaikin <150174304+poproshaikin@users.noreply.github.com>
2024-03-28 19:13:06 +0100

    Added navigation to product page

--- src/Components/HomeProductItem.tsx
+++ src/Components/HomeProductItem.tsx
@@ -1,25 +1,29 @@
 import Product from "../Models/Product";
 import "./HomeProductItem.css"
+import {config} from "../config";
+import {useNavigate} from "react-router-dom";
 
 interface props {
     product:Product
 }
 
 export default function HomeProductItem({product}:props) {
+    const navigate = useNavigate();
+
     return (
-        <div className="home-product-container" onClick={onClick}>
-            <div className="home-product-image-container">
-                <img className="home-product-image"
+        <div className="home-product-item-container" onClick={rerouteToProductPage}>
+            <div className="home-product-item-image-container">
+                <img className="home-product-item-image"
                      src={"data:image/png;base64," + product.imageData}/>
             </div>
-            <div className="home-product-text-container">
+            <div className="home-product-item-text-container">
                 <h3><strong>{product.name}</strong></h3>
-                <h2 className="home-product-price">{product.price}</h2>
+                <h2 className="home-product-item-price">{product.price} {config.currencyType}</h2>
             </div>
         </div>
     )
 
-    function onClick() {
-        console.log('clicked');
+    function rerouteToProductPage() {
+        navigate(`/product/${product.slug}`)
     }
 }
