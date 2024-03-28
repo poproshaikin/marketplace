4ccbd04694af9ea60ccb4f7b90756c469e29283e
poproshaikin <150174304+poproshaikin@users.noreply.github.com>
2024-03-28 19:13:14 +0100

    Added currency type

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,11 @@
 export const config = {
     serverIp: "10.0.0.24",
     serverPort: 5140,
-}
 
-export function blobToUrl(imageData:string) {
-    const blob = new Blob([imageData], { type: 'image/png' }); // Укажите правильный MIME-тип, соответствующий формату изображения
-
-    return URL.createObjectURL(blob);
+    currencyType: "Kč"
 }
+// export function blobToUrl(imageData:string) {
+//     const blob = new Blob([imageData], { type: 'image/png' }); // Укажите правильный MIME-тип, соответствующий формату изображения
+//
+//     return URL.createObjectURL(blob);
+// }
